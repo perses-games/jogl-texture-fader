4fff1b9137f1e2b61387fa12f966a898b61930a2
rnentjes <rien@nentjes.com>
2014-01-26 12:24:10 +0100

    Scaling & translation & rotation working

--- src/com/persesgames/jogl/jogl/Renderer.java
+++ src/com/persesgames/jogl/jogl/Renderer.java
@@ -40,13 +40,8 @@ public class Renderer implements GLEventListener  {
              0.0f,  1.0f,
     };
 
-    private float []                projectionMatrix;
-    private float []                modelViewMatrix = {
-            1.0f,0.0f,0.0f,0.0f,
-            0.0f,1.0f,0.0f,0.0f,
-            0.0f,0.0f,1.0f,0.0f,
-            0.0f,0.0f,0.0f,1.0f,
-    };
+    private Matrix                  projectionMatrix = new Matrix();
+    private Matrix                  modelViewMatrix  = new Matrix();
 
     private FloatBuffer             fbTxtVertices       = Buffers.newDirectFloatBuffer(txtVerts);
 
@@ -75,168 +70,7 @@ public class Renderer implements GLEventListener  {
         this.keyboard = keyboard;
 
         aspect = 1920f/1080f;
-        this.projectionMatrix = setPerspectiveProjection(90f, aspect, 1.0f, 50.0f);
-    }
-
-    public void setIdentityMatrix(float [] matrix) {
-        assert matrix.length == 16;
-
-        matrix[ 0] = 1.0f;
-        matrix[ 1] = 0.0f;
-        matrix[ 2] = 0.0f;
-        matrix[ 3] = 0.0f;
-        matrix[ 4] = 0.0f;
-        matrix[ 5] = 1.0f;
-        matrix[ 6] = 0.0f;
-        matrix[ 7] = 0.0f;
-        matrix[ 8] = 0.0f;
-        matrix[ 9] = 0.0f;
-        matrix[10] = 1.0f;
-        matrix[11] = 0.0f;
-        matrix[12] = 0.0f;
-        matrix[13] = 0.0f;
-        matrix[14] = 0.0f;
-        matrix[15] = 1.0f;
-    }
-
-    public void mul4x4(float [] a, float [] b, float [] result) {
-        assert a.length == 16;
-        assert b.length == 16;
-        assert result.length == 16;
-
-        result[ 0] =   a[ 0] * b[ 0] + a[ 1] * b[ 4] + a[ 2] * b[ 8] + a[ 3] * b[12];
-        result[ 1] =   a[ 0] * b[ 1] + a[ 1] * b[ 5] + a[ 2] * b[ 9] + a[ 3] * b[13];
-        result[ 2] =   a[ 0] * b[ 2] + a[ 1] * b[ 6] + a[ 2] * b[10] + a[ 3] * b[14];
-        result[ 3] =   a[ 0] * b[ 3] + a[ 1] * b[ 7] + a[ 2] * b[11] + a[ 3] * b[15];
-        result[ 4] =   a[ 4] * b[ 0] + a[ 5] * b[ 4] + a[ 6] * b[ 8] + a[ 7] * b[12];
-        result[ 5] =   a[ 4] * b[ 1] + a[ 5] * b[ 5] + a[ 6] * b[ 9] + a[ 7] * b[13];
-        result[ 6] =   a[ 4] * b[ 2] + a[ 5] * b[ 6] + a[ 6] * b[10] + a[ 7] * b[14];
-        result[ 7] =   a[ 4] * b[ 3] + a[ 5] * b[ 7] + a[ 6] * b[11] + a[ 7] * b[15];
-        result[ 8] =   a[ 8] * b[ 0] + a[ 9] * b[ 4] + a[10] * b[ 8] + a[11] * b[12];
-        result[ 9] =   a[ 8] * b[ 1] + a[ 9] * b[ 5] + a[10] * b[ 9] + a[11] * b[13];
-        result[10] =   a[ 8] * b[ 2] + a[ 9] * b[ 6] + a[10] * b[10] + a[11] * b[14];
-        result[11] =   a[ 8] * b[ 3] + a[ 9] * b[ 7] + a[10] * b[11] + a[11] * b[15];
-        result[12] =   a[12] * b[ 0] + a[13] * b[ 4] + a[14] * b[ 8] + a[15] * b[12];
-        result[13] =   a[12] * b[ 1] + a[13] * b[ 5] + a[14] * b[ 9] + a[15] * b[13];
-        result[14] =   a[12] * b[ 2] + a[13] * b[ 6] + a[14] * b[10] + a[15] * b[14];
-        result[15] =   a[12] * b[ 3] + a[13] * b[ 7] + a[14] * b[11] + a[15] * b[15];
-    }
-
-    private float [] translateMatrix = new float [] {
-            1.0f, 0.0f, 0.0f, 0.0f,
-            0.0f, 1.0f, 0.0f, 0.0f,
-            0.0f, 0.0f, 1.0f, 0.0f,
-            0.0f, 0.0f, 0.0f, 1.0f,
-    };
-
-    public void translate(float [] matrix, float x, float y, float z) {
-        assert matrix.length == 16;
-
-        translateMatrix[12] = x;
-        translateMatrix[13] = y;
-        translateMatrix[14] = z;
-
-        mul4x4(matrix, translateMatrix, matrix);
-    }
-
-    private float [] scaleMatrix = new float [] {
-            1.0f, 0.0f, 0.0f, 0.0f,
-            0.0f, 1.0f, 0.0f, 0.0f,
-            0.0f, 0.0f, 1.0f, 0.0f,
-            0.0f, 0.0f, 0.0f, 1.0f,
-    };
-
-    public void scale(float [] matrix, float x, float y, float z) {
-        assert matrix.length == 16;
-
-        scaleMatrix[0] = x;
-        scaleMatrix[5] = y;
-        scaleMatrix[10] = z;
-
-        mul4x4(matrix, scaleMatrix, matrix);
-    }
-
-    private float [] rotateXMatrix = new float [] {
-            1.0f, 0.0f, 0.0f, 0.0f,
-            0.0f, 1.0f, 0.0f, 0.0f,
-            0.0f, 0.0f, 1.0f, 0.0f,
-            0.0f, 0.0f, 0.0f, 1.0f,
-    };
-
-    public void rotateX(float [] matrix, float angle) {
-        assert matrix.length == 16;
-
-        rotateXMatrix[5] = (float)Math.cos(angle);
-        rotateXMatrix[6] = (float)-Math.sin(angle);
-        rotateXMatrix[9] = (float)Math.sin(angle);
-        rotateXMatrix[10] = (float)Math.cos(angle);
-
-        mul4x4(matrix, rotateXMatrix, matrix);
-    }
-
-    private float [] rotateYMatrix = new float [] {
-            1.0f, 0.0f, 0.0f, 0.0f,
-            0.0f, 1.0f, 0.0f, 0.0f,
-            0.0f, 0.0f, 1.0f, 0.0f,
-            0.0f, 0.0f, 0.0f, 1.0f,
-    };
-
-    public void rotateY(float [] matrix, float angle) {
-        assert matrix.length == 16;
-
-        rotateYMatrix[ 0] = (float)Math.cos(angle);
-        rotateYMatrix[ 2] = (float)Math.sin(angle);
-        rotateYMatrix[ 8] = (float)-Math.sin(angle);
-        rotateYMatrix[10] = (float)Math.cos(angle);
-
-        mul4x4(matrix, rotateYMatrix, matrix);
-    }
-
-    private float [] rotateZMatrix = new float [] {
-            1.0f, 0.0f, 0.0f, 0.0f,
-            0.0f, 1.0f, 0.0f, 0.0f,
-            0.0f, 0.0f, 1.0f, 0.0f,
-            0.0f, 0.0f, 0.0f, 1.0f,
-    };
-
-    public void rotateZ(float [] matrix, float angle) {
-        assert matrix.length == 16;
-
-        rotateZMatrix[ 0] = (float)Math.cos(angle);
-        rotateZMatrix[ 1] = (float)Math.sin(angle);
-        rotateZMatrix[ 4] = (float)-Math.sin(angle);
-        rotateZMatrix[ 5] = (float)Math.cos(angle);
-
-        mul4x4(matrix, rotateZMatrix, matrix);
-    }
-
-    public float [] setPerspectiveProjection(float angle, float imageAspectRatio, float near, float far) {
-        float [] matrix = new float[16];
-
-        float r = (float) (angle / 180f * Math.PI);
-        float f = (float) (1.0f / Math.tan(r / 2.0f));
-
-        matrix[0] = f / imageAspectRatio;
-        matrix[1] = 0.0f;
-        matrix[2] = 0.0f;
-        matrix[3] = 0.0f;
-
-        matrix[4] = 0.0f;
-        matrix[5] = f;
-        matrix[6] = 0.0f;
-        matrix[7] = 0.0f;
-
-        matrix[8] = 0.0f;
-        matrix[9] = 0.0f;
-        matrix[10] = -(far + near) / (far - near);
-        matrix[11] = -1.0f;
-
-        matrix[12] = 0.0f;
-        matrix[13] = 0.0f;
-        matrix[14] = -(2.0f * far * near) / (far - near);
-        matrix[15] = 0.0f;
-
-        return matrix;
+        this.projectionMatrix.setPerspectiveProjection(90f, aspect, 1.0f, 50.0f);
     }
 
     public void stop() {
@@ -371,17 +205,16 @@ public class Renderer implements GLEventListener  {
 
         double time = (System.currentTimeMillis() - start) / 1000.0;
 
-        gl.glUniformMatrix4fv(uProjection, 1, false, projectionMatrix, 0);
+        gl.glUniformMatrix4fv(uProjection, 1, false, projectionMatrix.get(), 0);
 
-        setIdentityMatrix(modelViewMatrix);
-        //rotateZ(modelViewMatrix, (float)time);
-        scale(modelViewMatrix, aspect, 1, 1);
-        translate(modelViewMatrix, 0, 0, -1);
-
-        //translate(modelViewMatrix, 0, 0, -1);
+        modelViewMatrix.setToIdentity();
+        modelViewMatrix.scale(aspect, 1, 1);
+        modelViewMatrix.rotateZ((float)time*3);
+        modelViewMatrix.translate(0, 0, -2);
+        //modelViewMatrix.scale(aspect, 1, 1);
 
         gl.glUniform1f(uAlpha, 1.0f);
-        gl.glUniformMatrix4fv(uModelView , 1, false, modelViewMatrix,  0);
+        gl.glUniformMatrix4fv(uModelView , 1, false, modelViewMatrix.get(),  0);
 
         texture.bind(gl);
 
@@ -390,7 +223,7 @@ public class Renderer implements GLEventListener  {
         float alpha = (float) Math.abs(Math.sin(time));
 
         gl.glUniform1f(uAlpha, alpha);
-        gl.glUniformMatrix4fv(uModelView , 1, false, modelViewMatrix,  0);
+        gl.glUniformMatrix4fv(uModelView , 1, false, modelViewMatrix.get(),  0);
 
         texture2.bind(gl);
 
